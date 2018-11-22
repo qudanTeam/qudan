262a81ed133cf8e4b573fa06a0d63f8311deff9f
zhanghengdashuaibi <41098361+zhanghengdashuaibi@users.noreply.github.com>
2018-11-22 13:37:48 +0800

    远程配置中心关联项目

---
+++ config-server/src/main/java/com/zhangheng/config/server/ConfigServerApplication.java
@@ -0,0 +1,26 @@
+package com.zhangheng.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 蜡笔小新不爱吃青椒 on 2018/7/27.
+ */
+
+@SpringBootApplication
+@EnableConfigServer//开启config-server
+//@EnableDiscoveryClient//将其注册到服务中心
+@EnableEurekaClient
+@EnableAutoConfiguration
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+
+    }
+}
---
+++ config-server/src/main/java/com/zhangheng/config/server/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.zhangheng.config.server.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 蜡笔小新不爱吃青椒 on 2018/7/27.
+ * 测试读取远程配置中的配置
+ */
+
+@RestController
+public class TestController {
+
+//    @Value("${profile}")
+//    private String from;
+//
+//    @RequestMapping("/profile")
+//    public String from() {
+//        return this.from;
+//    }
+
+
+}
