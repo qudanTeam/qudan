a853b36a07e5abe92e2549066ec383e40cc23d81
zhanghengdashuaibi <41098361+zhanghengdashuaibi@users.noreply.github.com>
2018-11-22 12:38:35 +0800

    后台管理系统接口服务

---
+++ ms_qudan_bg/src/main/java/com/zhangheng/qingcloud/msqudan/MsQudanApplication.java
@@ -0,0 +1,28 @@
+package com.zhangheng.qingcloud.msqudan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by 蜡笔小新不爱吃青椒 on 2018/7/19.
+ */
+
+@SpringBootApplication
+@EnableEurekaClient//开启Eureka注册功能
+@EnableCircuitBreaker//开启断路器功能
+@EnableDiscoveryClient//开启服务注册功能
+@EnableFeignClients
+@EnableSwagger2
+//@Import({DynamicDataSourceRegister.class}) // 注册动态多数据源
+public class MsQudanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsQudanApplication.class, args);
+
+    }
+}
---
+++ ms_qudan_bg/src/main/java/com/zhangheng/qingcloud/msqudan/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.zhangheng.qingcloud.msqudan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfig
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zhangheng.qingcloud.msqudan.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+ 
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "趣单Resful API", "","1.0", "", "趣单","", null);
+        return apiInfo;
+    }
+}
