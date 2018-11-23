67b5a78ce85785cc1b3ca1b7348d859170d0b487
zhangheng <zhangheng@pateo.com.cn>
2018-11-23 13:46:15 +0800

    调整监控安全验证

--- config-server/src/main/java/com/zhangheng/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/zhangheng/config/server/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.zhangheng.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * Created by 蜡笔小新不爱吃青椒 on 2018/7/27.
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableConfigServer//开启config-server
 //@EnableDiscoveryClient//将其注册到服务中心
 @EnableEurekaClient
