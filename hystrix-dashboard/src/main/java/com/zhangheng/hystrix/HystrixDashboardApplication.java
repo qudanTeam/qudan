c656fb99cdac122f1f2963028ff886ee17aa8902
zhangheng <zhangheng@pateo.com.cn>
2018-11-23 18:08:25 +0800

    2.0版本的熔断监听

--- hystrix-dashboard/src/main/java/com/zhangheng/hystrix/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/zhangheng/hystrix/HystrixDashboardApplication.java
@@ -1,11 +1,15 @@
 package com.zhangheng.hystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by 蜡笔小新不爱吃青椒 on 2018/7/31.
@@ -15,9 +19,20 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableTurbine
 @EnableHystrixDashboard
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class HystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
