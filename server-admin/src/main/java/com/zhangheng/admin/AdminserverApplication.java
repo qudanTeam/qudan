5eef94e16d8f896e76d1b66c71499d223df04e7d
zhangheng <zhangheng@pateo.com.cn>
2018-11-23 11:48:01 +0800

    开启安全验证

--- server-admin/src/main/java/com/zhangheng/admin/AdminserverApplication.java
+++ server-admin/src/main/java/com/zhangheng/admin/AdminserverApplication.java
@@ -1,15 +1,15 @@
 package com.zhangheng.admin;
 
-//import com.hazelcast.config.Config;
-//import com.hazelcast.config.EvictionPolicy;
-//import com.hazelcast.config.ListConfig;
-//import com.hazelcast.config.MapConfig;
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Created by 蜡笔小新不爱吃青椒 on 2018/7/31.
@@ -21,17 +21,38 @@ import org.springframework.context.annotation.Configuration;
 @EnableAdminServer
 public class AdminserverApplication {
 
-//    @Bean
-//    public Config hazelcastConfig() {
-//        return new Config().setProperty("hazelcast.jmx", "true")
-//                .addMapConfig(new MapConfig("spring-boot-admin-application-store").setBackupCount(1)
-//                        .setEvictionPolicy(EvictionPolicy.NONE))
-//                .addListConfig(new ListConfig("spring-boot-admin-event-store").setBackupCount(1)
-//                        .setMaxSize(1000));
-//    }
-
-
     public static void main(String[] args) {
         SpringApplication.run(AdminserverApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+            successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringAntMatchers(
+                            adminContextPath + "/instances",
+                            adminContextPath + "/actuator/**"
+                    );
+        }
     }
 }
