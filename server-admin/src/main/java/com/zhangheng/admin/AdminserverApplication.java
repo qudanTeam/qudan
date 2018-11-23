396f8c6129419e1dd8c11e47a58dc2b248372fb5
zhangheng <zhangheng@pateo.com.cn>
2018-11-23 11:58:44 +0800

    开启安全验证

--- server-admin/src/main/java/com/zhangheng/admin/AdminserverApplication.java
+++ server-admin/src/main/java/com/zhangheng/admin/AdminserverApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -25,6 +26,17 @@ public class AdminserverApplication {
         SpringApplication.run(AdminserverApplication.class, args);
     }
 
+    @Profile("insecure")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()//
+                    .and().csrf().disable();
+        }
+    }
+
+    @Profile("secure")
     @Configuration
     public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
         private final String adminContextPath;
