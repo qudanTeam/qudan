27bd7706aab5d09fbd67a7f21c37ff448b2a5ef7
exg <liuzhi@moore.ren>
2018-12-03 00:29:29 +0800

    注册接口，修改，服务无法调用

--- ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/cloudClients/hystrix/UserLoginHystrix.java
+++ ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/cloudClients/hystrix/UserLoginHystrix.java
@@ -1,8 +1,11 @@
 package com.qudan.qingcloud.msqudan.cloudClients.hystrix;
 
 import com.qudan.qingcloud.msqudan.cloudClients.UserLoginClient;
+import com.qudan.qingcloud.msqudan.controller.UserController;
 import com.qudan.qingcloud.msqudan.util.YHResult;
 import io.swagger.models.auth.In;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class UserLoginHystrix implements UserLoginClient {
 
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
     @Override
     public YHResult appLogin(String username, Integer userId) {
+
+        log.info("UserLoginHystrix  已执行!");
         return null;
     }
 }
