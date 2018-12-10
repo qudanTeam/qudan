dc87a8bd1cc71790a44d3c7972cdcb86ca158f10
zhangheng <zhangheng@pateo.com.cn>
2018-12-10 17:21:26 +0800

    扩展刷卡支付

--- ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/service/Impl/WxPayServiceImpl.java
+++ ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/service/Impl/WxPayServiceImpl.java
@@ -1,13 +1,20 @@
 package com.qudan.qingcloud.msqudan.service.Impl;
 
 import com.github.wxpay.sdk.WXPay;
+import com.github.wxpay.sdk.WXPayConstants;
 import com.github.wxpay.sdk.WXPayUtil;
 import com.qudan.qingcloud.msqudan.util.MD5Util;
 import com.qudan.qingcloud.msqudan.wxpay.MyWXConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import sun.misc.BASE64Decoder;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,6 +28,18 @@ import java.util.Map;
 public class WxPayServiceImpl {
 
     private final Logger logger = LoggerFactory.getLogger(WxPayServiceImpl.class);
+
+    /** 用户支付中，需要输入密码 */
+    private static final String ERR_CODE_USERPAYING = "USERPAYING";
+    private static final String ERR_CODE_AUTHCODEEXPIRE = "AUTHCODEEXPIRE";
+    /** 交易状态: 未支付 */
+    private static final String TRADE_STATE_NOTPAY = "NOTPAY";
+    /** 用户输入密码，尝试30秒内去查询支付结果 */
+    private static Integer remainingTimeMs = 10000;
+    /** 密钥算法 */
+    private static final String ALGORITHM = "AES";
+    /** 加解密算法/工作模式/填充方式 */
+    private static final String ALGORITHM_MODE_PADDING = "AES/ECB/PKCS5Padding";
 
     /**
      * 统一下单
@@ -49,7 +68,7 @@ public class WxPayServiceImpl {
         data.put("total_fee", total_fee);
         data.put("spbill_create_ip",spbill_create_ip);
         //异步通知地址（请注意必须是外网）
-        data.put("notify_url", "http://47.99.242.122:8763/wxpay/notify");
+        data.put("notify_url", "http://47.99.242.122:8763/msqudan/api/wxpay/notify");
         data.put("nonce_str",WXPayUtil.generateNonceStr());
         /**
          * 交易类型 JSAPI、NATIVE、APP
@@ -164,7 +183,7 @@ public class WxPayServiceImpl {
      * 订单查询
      * @param out_trade_no 订单号
      */
-    public Map<String, String> orderQuery(String  out_trade_no){
+    public Map<String, String> orderQuery(String out_trade_no){
         logger.info("订单查询...");
         MyWXConfig config = null;
         try {
@@ -355,6 +374,135 @@ public class WxPayServiceImpl {
             e.printStackTrace();
             logger.error("下载对账单异常返回:"+e.getMessage());
         }
+    }
+
+    /**
+     * 刷卡支付
+     * 注意：该方法没有处理return_code=FAIL的情况，暂时不考虑网络问题，这种情况直接返回错误
+     */
+    public Map<String, String> microPayWithPOS(Map<String, String> reqData) throws Exception {
+        logger.info("刷卡支付...");
+        MyWXConfig config = null;
+        try {
+            config = new MyWXConfig();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("单例MyWXConfig-------》异常==>刷卡支付");
+            logger.info(e.getMessage());
+        }
+        WXPay wxpay = new WXPay(config);
+        // 开始时间(毫秒)
+        long startTimestampMs = System.currentTimeMillis();
+
+        Map<String, String> responseMapForPay = wxpay.microPay(reqData);
+        logger.info(responseMapForPay.toString());
+
+        // // 先判断 协议字段返回(return_code)，再判断 业务返回，最后判断 交易状态(trade_state)
+        // 通信标识，非交易标识
+        String returnCode = responseMapForPay.get("return_code");
+        if (WXPayConstants.SUCCESS.equals(returnCode)) {
+            String errCode = responseMapForPay.get("err_code");
+            // 余额不足，信用卡失效
+            if (ERR_CODE_USERPAYING.equals(errCode) || "SYSTEMERROR".equals(errCode) || "BANKERROR".equals(errCode)) {
+                Map<String, String> orderQueryMap = null;
+                Map<String, String> requestData = new HashMap<>();
+                requestData.put("out_trade_no", reqData.get("out_trade_no"));
+
+                // 用户支付中，需要输入密码或系统错误则去重新查询订单API err_code, result_code, err_code_des
+                // 每次循环时的当前系统时间 - 开始时记录的时间 > 设定的30秒时间就退出
+                while (System.currentTimeMillis() - startTimestampMs < remainingTimeMs) {
+                    // 商户收银台得到USERPAYING状态后，经过商户后台系统调用【查询订单API】查询实际支付结果。
+                    orderQueryMap = wxpay.orderQuery(requestData);
+                    String returnCodeForQuery = orderQueryMap.get("return_code");
+                    if (WXPayConstants.SUCCESS.equals(returnCodeForQuery)) {
+                        // 通讯成功
+                        String tradeState = orderQueryMap.get("trade_state");
+                        if (WXPayConstants.SUCCESS.equals(tradeState)) {
+                            // 如果成功了直接将查询结果返回
+                            return orderQueryMap;
+                        }
+                        // 如果支付结果仍为USERPAYING，则每隔5秒循环调用【查询订单API】判断实际支付结果
+                        Thread.sleep(1000);
+                    }
+                }
+
+                // 如果用户取消支付或累计30秒用户都未支付，商户收银台退出查询流程后继续调用【撤销订单API】撤销支付交易。
+                String tradeState = orderQueryMap.get("trade_state");
+                if (TRADE_STATE_NOTPAY.equals(tradeState) || ERR_CODE_USERPAYING.equals(tradeState) || ERR_CODE_AUTHCODEEXPIRE.equals(tradeState)) {
+                    Map<String, String> reverseMap = wxpay.reverse(requestData);
+                    String returnCodeForReverse = reverseMap.get("return_code");
+                    String resultCode = reverseMap.get("result_code");
+                    if (WXPayConstants.SUCCESS.equals(returnCodeForReverse) && WXPayConstants.SUCCESS.equals(resultCode)) {
+                        // 如果撤销成功，需要告诉客户端已经撤销订单了
+                        responseMapForPay.put("err_code_des", "用户取消支付或尚未支付，后台已经撤销该订单，请重新支付！");
+                    }
+                }
+            }
+        }
+
+        return responseMapForPay;
+    }
+
+    /**
+     * 从request的inputStream中获取参数
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public Map<String, String> getNotifyParameter(HttpServletRequest request) throws Exception {
+        InputStream inputStream = request.getInputStream();
+        ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length = 0;
+        while ((length = inputStream.read(buffer)) != -1) {
+            outSteam.write(buffer, 0, length);
+        }
+        outSteam.close();
+        inputStream.close();
+
+        // 获取微信调用我们notify_url的返回信息
+        String resultXml = new String(outSteam.toByteArray(), "utf-8");
+        Map<String, String> notifyMap = WXPayUtil.xmlToMap(resultXml);
+
+        return notifyMap;
+    }
+    /**
+     * 解密退款通知
+     * 刷卡支付的
+     * <a href="https://pay.weixin.qq.com/wiki/doc/api/micropay.php?chapter=9_16&index=11>退款结果通知文档</a>
+     * @return
+     * @throws Exception
+     */
+    public Map<String, String> decodeRefundNotify(HttpServletRequest request) throws Exception {
+        logger.info("解密退款通知...");
+        MyWXConfig config = null;
+        try {
+            config = new MyWXConfig();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("单例MyWXConfig-------》异常==>解密退款通知");
+            logger.info(e.getMessage());
+        }
+        WXPay wxpay = new WXPay(config);
+        // 从request的流中获取参数
+        Map<String, String> notifyMap = this.getNotifyParameter(request);
+        logger.info(notifyMap.toString());
+
+        String reqInfo = notifyMap.get("req_info");
+        //（1）对加密串A做base64解码，得到加密串B
+        byte[] bytes = new BASE64Decoder().decodeBuffer(reqInfo);
+
+        //（2）对商户key做md5，得到32位小写key* ( key设置路径：微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置 )
+        Cipher cipher = Cipher.getInstance(ALGORITHM_MODE_PADDING);
+        SecretKeySpec key = new SecretKeySpec(WXPayUtil.MD5(config.getKey()).toLowerCase().getBytes(), ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        //（3）用key*对加密串B做AES-256-ECB解密（PKCS7Padding）
+        // java.security.InvalidKeyException: Illegal key size or default parameters
+        // https://www.cnblogs.com/yaks/p/5608358.html
+        String responseXml = new String(cipher.doFinal(bytes),"UTF-8");
+        Map<String, String> responseMap = WXPayUtil.xmlToMap(responseXml);
+        return responseMap;
     }
 
 }
