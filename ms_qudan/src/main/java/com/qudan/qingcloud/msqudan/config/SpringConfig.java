93ae1f6e43dc59526d272ea3bc2d8040b531b41f
exg <liuzhi@moore.ren>
2018-12-24 20:00:05 +0800

    wechat 支持跨域

--- ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/config/SpringConfig.java
+++ ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/config/SpringConfig.java
@@ -18,7 +18,7 @@ public class SpringConfig {
     @Bean
     public FilterRegistrationBean crossFilterregistration() {
         final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.addUrlPatterns("/msqudan/api/*");
+        registrationBean.addUrlPatterns("/msqudan/api/*","/wechat/*");
         registrationBean.addInitParameter("cors.allowed.headers","DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,Accept,Access-Control-Request-Method,Access-Control-Request-Headers");
         registrationBean.addInitParameter("cors.allowed.origins","*");
         registrationBean.addInitParameter("cors.allowed.methods","POST, GET, OPTIONS, PUT, DELETE, HEAD");
