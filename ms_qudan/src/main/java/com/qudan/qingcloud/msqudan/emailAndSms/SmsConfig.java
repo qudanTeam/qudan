4930c0ee896eddfb8c86af85404e19281c879c3b
exg <liuzhi@moore.ren>
2019-01-23 18:24:34 +0800

    smsconfig 增加 @Configuration

--- ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/emailAndSms/SmsConfig.java
+++ ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/emailAndSms/SmsConfig.java
@@ -6,12 +6,14 @@ import com.qudan.qingcloud.msqudan.emailAndSms.sms.SmsServiceSubmail;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by word on 2017/4/20.
  */
 @Repository
+@Configuration
 public class SmsConfig {
     //SUBMAIL 短信  以下4个
     @Value("${sms.submail.xsend_url}")//发送短信
