4eab7121c34f869b0bc2b6ee4b068055a94cb19b
lzguxiandu <lzguxiandu@163.com>
2019-02-12 16:41:06 +0800

    时间格式修改

--- ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/util/responses/OrderVos.java
+++ ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/util/responses/OrderVos.java
@@ -27,7 +27,7 @@ public class OrderVos {
     private String expressName;
     private String expressNo;
 
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date applyTime;
     private BigDecimal award;
 
