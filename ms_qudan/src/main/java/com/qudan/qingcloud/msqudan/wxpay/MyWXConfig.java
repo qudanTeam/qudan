e9e18c12edd1cdf6268d1e4ef5922ef9e6268adf
exg <liuzhi@moore.ren>
2019-03-30 18:50:32 +0800

    增加趣单生活的配置

--- ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/wxpay/MyWXConfig.java
+++ ms_qudan/src/main/java/com/qudan/qingcloud/msqudan/wxpay/MyWXConfig.java
@@ -5,13 +5,15 @@ import org.bouncycastle.util.test.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
 import java.io.*;
 
 /**
  * Created by 蜡笔小新不爱吃青椒 on 2018/12/10.
  */
-//@Configuration
+@Configuration
 public class MyWXConfig implements WXPayConfig {
 
 //    @Value("${appId}")
@@ -26,6 +28,7 @@ public class MyWXConfig implements WXPayConfig {
 //    @Value("${wxpath}")
 //    private String wxpath;
 
+
     private String appId = "wx17dd8d8d9de5910e";//appId
     private String  mchId = "1524656921";//商户号
     private String key = "gGq1265zbb6743lH5672019JSynb8tg8";//密钥key
@@ -35,28 +38,30 @@ public class MyWXConfig implements WXPayConfig {
     private static MyWXConfig INSTANCE;
 
     public MyWXConfig() throws Exception {
+        String env = System.getProperty("spring.profiles.active");
+        String wxDeveloper = System.getProperty("wx.developer");
+        InputStream certStream= null;
+        if(!StringUtils.isEmpty(wxDeveloper) && wxDeveloper.equals("pro-sh")){
+            appId = "wx5937eccb3b4a99d7";
+            mchId = "1520779301";
+            key = "g317g180q3355G2233G3344Q998ggq88";
+            certStream=Test.class.getResourceAsStream("/wxproconfig/apiclient_cert_sh.p12");
+        } else {
+            appId = "wx17dd8d8d9de5910e";
+            mchId = "1524656921";
+            key = "gGq1265zbb6743lH5672019JSynb8tg8";
+            certStream=Test.class.getResourceAsStream("/wxproconfig/apiclient_cert.p12");
+        }
         //证书位置(本地读法)
 //        File file = (ResourceUtils.getFile("classpath:wxconfig/apiclient_cert.p12"));
 //        InputStream certStream = new FileInputStream(file);
 //        this.certData = new byte[(int) file.length()];
         //打包后的读法
-        InputStream certStream=Test.class.getResourceAsStream("/wxproconfig/apiclient_cert.p12");
+//        InputStream certStream=Test.class.getResourceAsStream("/wxproconfig/apiclient_cert.p12");
 //        InputStream certStream=Test.class.getResourceAsStream(wxpath);
         this.certData = new byte[certStream.available()];
         certStream.read(this.certData);
         certStream.close();
-    }
-
-    //单例处理
-    public static MyWXConfig getInstance() throws Exception{
-        if (INSTANCE == null) {
-            synchronized (MyWXConfig.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = new MyWXConfig();
-                }
-            }
-        }
-        return INSTANCE;
     }
 
     public String getAppID() {
